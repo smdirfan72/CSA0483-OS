aafdf2a99b0a77e9fb4c2a0d942482c6f31d5b41
smdirfan72 <123714668+smdirfan72@users.noreply.github.com>
2023-11-02 14:03:00 +0530

    Create 5th Program OS .c

---
+++ 5th Program OS .c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+struct Process {
+    int process_id;
+    int burst_time;
+    int waiting_time;
+    int turnaround_time;
+};
+
+void calculateTimes(struct Process processes[], int n) {
+    int total_waiting_time = 0;
+    int total_turnaround_time = 0;
+
+    processes[0].waiting_time = 0;
+    processes[0].turnaround_time = processes[0].burst_time;
+
+    for (int i = 1; i < n; i++) {
+        processes[i].waiting_time = processes[i - 1].waiting_time + processes[i - 1].burst_time;
+        processes[i].turnaround_time = processes[i].waiting_time + processes[i].burst_time;
+
+        total_waiting_time += processes[i].waiting_time;
+        total_turnaround_time += processes[i].turnaround_time;
+    }
+
+    double avg_waiting_time = (double)total_waiting_time / n;
+    double avg_turnaround_time = (double)total_turnaround_time / n;
+
+    printf("\nProcess\tBurst Time\tWaiting Time\tTurnaround Time\n");
+    for (int i = 0; i < n; i++) {
+        printf("%d\t%d\t\t%d\t\t%d\n",
+               processes[i].process_id, processes[i].burst_time,
+               processes[i].waiting_time, processes[i].turnaround_time);
+    }
+    printf("\nAverage Waiting Time: %.2f\n", avg_waiting_time);
+    printf("Average Turnaround Time: %.2f\n", avg_turnaround_time);
+}
+
+void sortProcessesByBurstTime(struct Process processes[], int n) {
+    for (int i = 0; i < n - 1; i++) {
+        for (int j = i + 1; j < n; j++) {
+            if (processes[i].burst_time > processes[j].burst_time) {
+                struct Process temp = processes[i];
+                processes[i] = processes[j];
+                processes[j] = temp;
+            }
+        }
+    }
+}
+
+int main() {
+    int n;
+    printf("Enter the number of processes: ");
+    scanf("%d", &n);
+
+    struct Process processes[n];
+
+    for (int i = 0; i < n; i++) {
+        processes[i].process_id = i + 1;
+        printf("Enter burst time for Process %d: ", i + 1);
+        scanf("%d", &processes[i].burst_time);
+    }
+
+    sortProcessesByBurstTime(processes, n);
+
+    calculateTimes(processes, n);
+
+    return 0;
+}
